b495c2046316d6229eb5d0cc7012d5acb0c0ef6b
nrusk <nrusk@ivytech.edu>
2024-10-01 21:13:43 -0400

    1

--- Lab5/Lab5.cpp
+++ Lab5/Lab5.cpp
@@ -1,20 +1,43 @@
-// Lab5.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
-
 #include <iostream>
+#include <vector>
+#include <string>
 
-int main()
-{
-    std::cout << "Hello World!\n";
+
+using namespace std;
+
+static char Cypher(char& c) {
+
+    vector<char> cypherVector{ 'V','F','X','B','L','I','T','Z','J','R','P','H','D','K','N','O','W','S','G','U','Y','Q','M','A','C','E' };
+
+    if (c >= 65 && c <= 90) {                   //is a capital letter
+        return (c = (cypherVector[c - 65]));
+    }
+    else if (c >= 97 && c <= 122) {             //is a lower case letter
+        c = (c - 32);                           //convert to upper case
+        c = (cypherVector[c - 65]);             //switch with cypher letter
+        return (c = (c + 32));                  //convert back to lower case
+    }
+    else {
+        return c;                               //not a letter
+    }
 }
 
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
+int main() {
 
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
+    vector<char> vector2;
+    string inputText;
+    string message;
+
+    cout << "Enter some text for the cypher: ";
+    getline(cin, inputText);
+
+    for (char c : inputText) {
+        Cypher(c);
+        message += c;
+        
+    }
+
+    cout << "Encoded message: " << message;
+}
+
+
